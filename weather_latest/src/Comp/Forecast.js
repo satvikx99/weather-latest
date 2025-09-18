76e4d3af69016fa6ea17e17b1e4e0cebd2ac53f2
shazam <satvikneolia169@gmail.com>
2025-09-18 15:42:20 +0530

    Add files via upload

---
+++ weather_latest/src/Comp/Forecast.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Forecast() {
+  return (
+    <div className='my-4'>
+      <h5>Forecast</h5>
+    </div>
+  )
+}
+
+export default Forecast
---
+++ weather_latest/src/Comp/Head.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Head() {
+  return (
+    <div className="container-fluid text-center my-4 " >
+        <h1 style={{display : 'inline'}}>The Weather App </h1>
+    </div>
+
+  )
+}
+
